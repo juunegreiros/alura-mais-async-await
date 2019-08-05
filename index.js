8caed987aa91daee925169a21ce9b26ecd53d76d
Ju <ju_bsnegreiros@hotmail.com>
2019-08-05 14:16:01 -0300

    Adiciona async/await

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ const apiUrl = 'http://localhost:5000/clientes'
 const clientes = [{ nome: 'teste', cpf: '00000000000' }]
 const listaClientes = document.querySelector('#listaClientes')
 
-clientes.forEach(cliente => listaClientes.innerHTML += `<li>nome: ${cliente.nome} cpf: ${cliente.cpf} </li>`)
+async function buscaClientes() {
+  const resposta = await fetch(apiUrl)
+  const novosClientes = await resposta.json()
+
+  if(resposta.status != 200) {
+    throw resposta
+  }
+  return novosClientes
+}
+
+buscaClientes()
+  .then(novosClientes => novosClientes.forEach(cliente => 
+    listaClientes.innerHTML += `<li>nome: ${cliente.nome} cpf: ${cliente.cpf} </li>`
+  ))
+  .catch(erro => listaClientes.innerHTML = erro)
